c051b744d81e93704873807aa1ca4ddac51c717c
Laurent Perrinet <laurent.perrinet@gmail.com>
2015-09-23 12:21:43 +0200

    scenario line contraint

--- scenario_line.py
+++ scenario_line.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf8 -*-
 
+"""
+
+Sur une ligne de lames, on fait tourner les lames avec un mouvement émergent.
+
+"""
+
+
 import elasticite as el
 
 e = el.EdgeGrid(N_lame=20, grid_type='line')
--- scenario_line_contraint.py
+++ scenario_line_contraint.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf8 -*-
 
+"""
+
+Sur une ligne de lames, on fait tourner les lames avec un mouvement relativement élastique mais exogene (prédeterminé, pas émergent)
+
+"""
+
 import elasticite as el
 import numpy as np
 class EdgeGrid(el.EdgeGrid):
@@ -12,7 +18,6 @@ class EdgeGrid(el.EdgeGrid):
         force = angle(self.t, self.lames[0, :]) - self.lames[2, :]
         force -= damp(self.t) * self.lames[3, :]/self.dt
         return 10. * force
-
 
 e = EdgeGrid(N_lame=20, grid_type='line')
 el.main(e)
