29bdc04d1f9abf60b99946772e90e1833de83800
Laurent Perrinet <laurent.perrinet@gmail.com>
2015-09-23 12:19:02 +0200

    scenario line contraint

--- scenario_line_contraint.py
+++ scenario_line_contraint.py
@@ -2,6 +2,17 @@
 # -*- coding: utf8 -*-
 
 import elasticite as el
+import numpy as np
+class EdgeGrid(el.EdgeGrid):
+    def champ(self):
+        force = np.zeros_like(self.lames[2, :])
+        angle = lambda t, x: np.pi*np.mod(t / 6.-x, 1.)
+        damp = lambda t: 0.01
 
-e = el.EdgeGrid(N_lame=20, grid_type='line')
+        force = angle(self.t, self.lames[0, :]) - self.lames[2, :]
+        force -= damp(self.t) * self.lames[3, :]/self.dt
+        return 10. * force
+
+
+e = EdgeGrid(N_lame=20, grid_type='line')
 el.main(e)
