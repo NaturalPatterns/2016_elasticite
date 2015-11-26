cfd8045264a89c6f5d17912099bb64c38d27d591
Laurent Perrinet <laurent.perrinet@gmail.com>
2015-11-26 14:55:38 +0100

    scenario vague

---
+++ posts/scenario_line_vague.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+
+def make_vague():
+    name = 'waves'
+    import os
+    import numpy as np
+    import MotionClouds as mc
+    mc.N_X, mc.N_Y, mc.N_frame = 128, 32, 256
+
+    fx, fy, ft = mc.get_grids(mc.N_X, mc.N_Y, mc.N_frame)
+    vext = '.webm'
+    theta, B_theta, B_wave = 0., np.pi/16., .1
+    alpha, sf_0, B_sf, B_V = 2., .25, .3, 2.
+    seed = 1234565
+    V_X, V_Y, g = .5, 0., .1
+    loggabor=True
+
+    def envelope_gravity(fx, fy, ft, B_wave, g=.1):
+        """
+         Gravitational envelope:
+         selects the plane corresponding to the speed (V_X, V_Y) with some thickness B_V
+
+        """
+        k = fx*V_X+fy*V_Y
+        env = np.exp(-.5*(((ft/.5)**2-g*np.sqrt(((k/.5)**2)))**2/(B_wave*mc.frequency_radius(fx, fy, ft, ft_0=np.inf))**2))
+        env *= (ft*k) < 0
+        return env
+
+    def envelope_gabor_wave(fx, fy, ft, B_wave, V_X=mc.V_X, V_Y=mc.V_Y,
+                            B_V=mc.B_V, B_v=1., sf_0=mc.sf_0, B_sf=mc.B_sf, loggabor=mc.loggabor,
+                            theta=mc.theta, B_theta=mc.B_theta, alpha=mc.alpha):
+        """
+        Returns the Motion Cloud kernel
+
+        """
+        envelope = mc.envelope_gabor(fx, fy, ft, V_X=V_X, V_Y=V_Y,
+                                     B_V=B_V, sf_0=sf_0, B_sf=B_sf, loggabor=loggabor,
+                                     theta=theta, B_theta=B_theta, alpha=alpha)
+        envelope *= envelope_gravity(fx, fy, ft, B_wave=B_wave)
+        return envelope
+
+    B_v_low, B_v_high = .025, .1
+
+    mc_vague = envelope_gabor_wave(fx, fy, ft, V_X=1., V_Y=0., B_wave=B_v_low, B_V=B_V, theta=theta, B_theta=B_theta, sf_0=sf_0, B_sf=B_sf, alpha=alpha)
+    vague_dense = mc.rectif(mc.random_cloud(mc_vague))
+    vague_solo = mc.rectif(mc.random_cloud(mc_vague, seed=seed, impulse=True))
+    return vague_dense, vague_solo
+
+import elasticite as el
+import numpy as np
+
+class EdgeGrid(el.EdgeGrid):
+    def __init__(self, z, x_offset=0, y_offset=0, t_offset=0, N_steps = 256, **kwargs):
+        #super(el.EdgeGrid.__init__(self))
+        #super(el.EdgeGrid, self).__init__(**kwargs)
+        el.EdgeGrid.__init__(self, **kwargs)
+        print (self.verb, kwargs)
+        self.z = z
+        self.x_offset = x_offset
+        self.y_offset = y_offset
+        self.t_offset = t_offset
+        self.N_steps = N_steps
+        print(self.x_offset, self.y_offset, self.t_offset)
+        print(self.z.shape)
+        
+        
+    def update(self):
+        if self.structure: N_lame = self.N_lame-self.struct_N
+        else: N_lame = self.N_lame
+        i = np.int(self.t/self.period * self.N_steps)
+        #print(self.x_offset)#, self.y_offset, self.t_offset)
+        #print(i)
+        #print(self.z.shape)
+        #self.lames[2, :N_lame] = self.z[self.x_offset:(self.x_offset+N_lame), self.y_offset, self.t_offset+i]
+        self.lames[2, :N_lame] = self.z[:N_lame, 0, i]
+        
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv)>1: mode = sys.argv[1]
+    else: mode = 'both'
+        
+    vague_dense, vague_solo = make_vague()
+
+    e = EdgeGrid(N_lame=25, grid_type='line', mode=mode,
+                 verb=True, filename='mat/line_vague.npy', period=20., 
+                 z = vague_dense,
+                 x_offset=0, y_offset=0, t_offset=0, N_steps=512)
+
+    # running the code
+    el.main(e)
+
+    e = EdgeGrid(N_lame=25, grid_type='line', mode=mode,
+                 verb=True, filename='mat/line_vague.npy', period=20., 
+                 z = vague_solo,
+                 x_offset=vague_solo.shape[0]//2-25//2, y_offset=vague_solo.shape[1]//2, 
+                 t_offset=vague_solo.shape[-1]*3//8, N_steps=vague_solo.shape[-1]//4)
+
+    # running the code
+    el.main(e)
