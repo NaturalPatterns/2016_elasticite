00d71fc96ffb113abf04a02fdd111739b434598a
Laurent Perrinet <laurent.perrinet@gmail.com>
2015-11-27 16:15:51 +0100

    scenario vague

--- scenario_line_vague.py
+++ scenario_line_vague.py
@@ -81,7 +81,7 @@ class EdgeGrid(el.EdgeGrid):
         dsurface = np.gradient(surface)
         dsurface *= np.bartlett(N_lame)
         #print(dsurface.mean(), dsurface.max(), damp(self.t))
-        #dsurface /= np.abs(dsurface).max()
+        dsurface /= np.abs(dsurface).max()
         dsurface *= np.tan(np.pi/8) # maximum angle achieved
         self.lames[2, :N_lame] = np.arctan(dsurface)*damp(self.t)
         
