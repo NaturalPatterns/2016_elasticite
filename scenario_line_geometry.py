29e255560787fe7da355935c9760eb6426766e0f
Laurent Perrinet <laurent.perrinet@gmail.com>
2015-11-30 16:11:58 +0100

    scenario geometry

--- scenario_line_geometry.py
+++ scenario_line_geometry.py
@@ -10,28 +10,32 @@ class EdgeGrid(el.EdgeGrid):
         else: N_lame = self.N_lame
 
         force = np.zeros_like(self.lames[2, :N_lame])
-        damp_min = 0.05
-        damp_tau = 1.5
+        damp_min = 0.2
+        damp_tau = 10.
         damp = lambda t: damp_min + (1.-damp_min)*np.exp(-np.abs(np.mod(t+self.period/2, self.period)-self.period/2)/damp_tau)
+
+        smooth = lambda t: 1.-np.exp(-np.abs(np.mod(t+self.period/2, self.period)-self.period/2)**2/damp_tau**2)
 
         damp_angle_tau = 8.
         damp_angle = lambda t: 1.-np.exp(-(np.mod(t+self.period/2, self.period)-self.period/2)**2/2/damp_angle_tau**2)
         xf = lambda t: location[0]
         zf = lambda t: location[2] + 3.5 * np.sin(2*np.pi*(t)/self.period)
 
-        noise = lambda t, x: .01 #* np.exp((np.cos(2*np.pi*((t-0.) / 6. + x))-1.)/ .1**2)
+        noise = lambda t: .8 * smooth(t)#* np.exp((np.cos(2*np.pi*((t-0.) / 6. + x))-1.)/ .1**2)
         #damp = lambda t: 0.001 #* np.exp(np.cos(t / 6.) / 3.**2)
-        colin_t = lambda t, y: -8.#*np.exp((np.cos(2*np.pi*((t-3.) / 6. + y))-1.)/ .3**2)
-        colin_d = lambda d: np.exp(-d**2/2/1.5**2) #np.exp(-np.log((d+1.e-12)/.05)**2/2/1.5)
+        colin_t = lambda t: -.2 * smooth(t) #*np.exp((np.cos(2*np.pi*((t-3.) / 6. + y))-1.)/ .3**2)
+        colin_d = lambda d: np.exp(-d**2/2/.25**2) #np.exp(-np.log((d+1.e-12)/.05)**2/2/1.5)
 
         #delta_angle = np.mod(self.angle_relatif()-np.pi/3., 2*np.pi/3)
         delta_angle = self.angle_relatif()[:N_lame, :N_lame]-np.pi/2.
         #delta_angle *= np.sign(delta_angle)
         D = self.distance(do_torus=True)[:N_lame, :N_lame]
-        force += colin_t(self.t, self.lames[1, :N_lame]) * np.sum(np.sin(2*delta_angle)*colin_d(D), axis=1)
-        force += noise(self.t, self.lames[0, :N_lame])*np.pi*np.random.randn(N_lame)
+        force  += colin_t(self.t) * np.sum(np.sin(2*delta_angle)*colin_d(D), axis=1) * smooth(self.t)
+        #force += force_/np.abs(force_**2).sum()
+        force -= 7.5 * (np.mod(self.lames[2, :N_lame]+np.pi/2, np.pi) - np.pi/2) * (1- smooth(self.t) )
+        force += noise(self.t)*np.pi*np.random.randn(N_lame)
         force -= damp(self.t) * self.lames[3, :N_lame]/self.dt
-        return 2. * force
+        return .01 * force
     
 
 if __name__ == "__main__":
