f4a5fc755f6ad1484e3df2f79c0e3d41d525bf55
Laurent Perrinet <laurent.perrinet@gmail.com>
2015-12-04 17:10:50 +0100

    scenario geometry  90deg

--- scenario_line_geometry_90deg.py
+++ scenario_line_geometry_90deg.py
@@ -10,17 +10,17 @@ class EdgeGrid(el.EdgeGrid):
         else: N_lame = self.N_lame
 
         force = np.zeros_like(self.lames[2, :N_lame])
-        damp_min = 0.4
+        damp_min = 0.7
         damp_tau = 14.
         damp = lambda t: damp_min + (1.-damp_min)*np.exp(-np.abs(np.mod(t+self.period/2, self.period)-self.period/2)/damp_tau)
 
         smooth = lambda t: 1.-np.exp(-np.abs(np.mod(t+self.period/2, self.period)-self.period/2)**2/damp_tau**2)
         on_off = lambda t, freq: (np.sin(2*np.pi*t/self.period*freq) > 0.)
 
-        noise = lambda t: .5 * smooth(t)
+        noise = lambda t: .2 * smooth(t)
         
-        force -= 7.5 * (np.mod(self.lames[2, :N_lame]+np.pi/2, np.pi) - np.pi/2 -np.pi/4) *smooth(self.t) *on_off(self.t, 3)
-        force -= 40 * (np.mod(self.lames[2, :N_lame]+np.pi/2, np.pi) - np.pi/2) * (1- smooth(self.t) ) *(1-on_off(self.t, 3))
+        force -= 7.5 * (np.mod(self.lames[2, :N_lame], np.pi) -np.pi/2 ) *smooth(self.t) * on_off(self.t, 3)
+        force -= 40 * (np.mod(self.lames[2, :N_lame]+np.pi/2, np.pi) - np.pi/2) * (1- smooth(self.t) ) * (1-on_off(self.t, 3))
         force += noise(self.t)*np.pi*np.random.randn(N_lame)
         force -= damp(self.t) * self.lames[3, :N_lame]/self.dt
         force *= .01
@@ -32,7 +32,7 @@ if __name__ == "__main__":
     if len(sys.argv)>1: mode = sys.argv[1]
     else: mode = 'both'
     filename = None
-    filename = 'mat/line_geometry_45deg.npy'
+    filename = 'mat/line_geometry_90deg.npy'
     e = EdgeGrid(N_lame=25, grid_type='line', mode=mode, verb=False, filename=filename, period=4*60.)
     el.main(e)
 
